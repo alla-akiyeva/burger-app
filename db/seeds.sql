dcf0c205b7444de3773a8f43a95a237610099e33
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-10-04 12:40:08 -0400

    basic folder sctructure and mysql db files

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+-- In this file, write insert queries to populate the burgers table with at least three entries.
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Elk Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Portobello Mushroom Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bison Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Wild Salmon Burger", FALSE);
